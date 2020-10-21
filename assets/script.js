7801a491ebad3cca54ac1d3714f0832cf6c96b8c
alberto de la cruz <albertocj54@gmail.com>
2020-10-20 12:41:53 -1200

    version 1

---
+++ assets/script.js
@@ -0,0 +1,11 @@
+window.addEventListener("load", function(){
+$(function(){
+  $('.bxslider').bxSlider({
+    auto: true,
+    autoControls: true,
+     stopAutoOnClick: true,
+  pager: true,
+  slideWidth: 600
+  });
+});
+});
